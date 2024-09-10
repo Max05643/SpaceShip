9d2a8e6b0e459771d45e00bdc282bd9c6d56377a
Maksym <05643mak@gmail.com>
2024-09-10 22:33:35 +0200

    Planet animation

--- Assets/Scripts/PlanetController.cs
+++ Assets/Scripts/PlanetController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class PlanetController : MonoBehaviour
@@ -10,10 +11,24 @@ public class PlanetController : MonoBehaviour
     [SerializeField]
     float distanceFromPlayer = 500;
 
+    [SerializeField]
+    float rotationTime = 120;
 
+
+    Tween currentAnimation;
+
+    void Start()
+    {
+        currentAnimation = transform.DORotate(Vector2.up * 180, rotationTime).SetLoops(-1, LoopType.Incremental).SetEase(Ease.Linear);
+    }
     void Update()
     {
         transform.position = playerTransform.position + Vector3.forward * distanceFromPlayer;
+    }
+
+    void OnDestroy()
+    {
+        currentAnimation?.Kill();
     }
 
 }
